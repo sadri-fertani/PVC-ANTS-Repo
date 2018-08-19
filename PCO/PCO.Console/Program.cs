6da2788527a2c83ab1eb82dd747e2d3d98d21af0
Sadri FERTANI <sadri.fertani@live.fr>
2018-08-19 09:21:59 +0200

    Init solution

---
+++ PCO/PCO.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PCO.Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
